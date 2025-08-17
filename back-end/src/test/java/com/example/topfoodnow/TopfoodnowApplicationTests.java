045417fab819a7e03e90be3a6ed51b42cf35d430
Kino <94521109+Kinokino00@users.noreply.github.com>
2025-08-17 14:33:14 +0800

    fix: Direct mock external service beans for CI testing

--- back-end/src/test/java/com/example/topfoodnow/TopfoodnowApplicationTests.java
+++ back-end/src/test/java/com/example/topfoodnow/TopfoodnowApplicationTests.java
@@ -1,17 +1,13 @@
 package com.example.topfoodnow;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@DataJpaTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-		"gcp.storage.credentials.location=classpath:test-gcs-key.json", // 假的路徑
-		"spring.cloud.gcp.project-id=test-project-id" // 假的專案ID
-})
 class TopfoodnowApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
