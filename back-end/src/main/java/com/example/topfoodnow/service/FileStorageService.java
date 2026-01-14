caa99d441b7cfeffd31fbfa2e54ae08cdf362147
TopFoodNow Developer <user@topfoodnow.com>
2026-01-14 21:37:05 +0800

    完成 develop 分支額外模組的重構
    - 移動 CategoryController 到 controller/category/
    - 移動 LogController 到 controller/log/
    - 移動 StoreCategoryController, StoreController 到 controller/store/
    - 移動 CategoryService 到 service/category/ 並建立 impl/
    - 移動 LogService 到 service/log/ 並建立 impl/
    - 移動 GcsService 到 service/gcs/ 並建立 impl/
    - 移動 JwtBlacklistService 到 service/jwtblacklist/ 並建立 impl/
    - 移動 StoreCategoryService 到 service/storecategory/ 並建立 impl/
    - 刪除舊的 service impl 資料夾和重複檔案

--- back-end/src/main/java/com/example/topfoodnow/service/FileStorageService.java
+++
@@ -1,9 +0,0 @@
-package com.example.topfoodnow.service;
-
-import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-
-public interface FileStorageService {
-    String uploadFile(MultipartFile file, String folderPath) throws IOException;
-    void deleteFile(String fileUrl);
-}
--- back-end/src/main/java/com/example/topfoodnow/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.topfoodnow.service;
-
-public interface MailService {
-    void sendVerificationEmail(String toEmail, String userName, String verificationCode);
-    void sendPasswordResetEmail(String toEmail, String userName, String resetLink);
-    void sendEmail(String toEmail, String subject, String content);
-}
